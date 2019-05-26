f5705dd6eec7d30d1cadb8f0a74d4781bad20e22
livalex <livalex11@gmail.com>
2019-05-26 14:21:37 +0300

    int sort

--- Lamda/Program.cs
+++ Lamda/Program.cs
@@ -10,6 +10,14 @@ namespace Lamda
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>() { -1, 10, 3, 99, 500, 0, 20 };
+
+            UniversalSort.Sort(ref list);
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
---
+++ Lamda/UniversalSort.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamda
+{
+    class UniversalSort
+    {
+        public static void Sort(ref List<int> _list) {
+
+            bool isSorted;
+            do
+            {
+                isSorted = true;
+                for (int i = 0; i < _list.Count - 1; i++)
+                {
+                    if (_list[i] < _list[i + 1])
+                    {
+                        int tmp = _list[i];
+                        _list[i] = _list[i + 1];
+                        _list[i + 1] = tmp;
+                        isSorted = false;
+                    }
+                }
+            } while (!isSorted);
+        }
+    }
+}
